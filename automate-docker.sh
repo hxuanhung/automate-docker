74e06f927ccd12158bdc431ea85421d062289bd2
hxuanhung <hxuanhung@gmail.com>
2017-10-23 11:04:04 +0200

    Change help messages

--- automate-docker.sh
+++ automate-docker.sh
@@ -2,36 +2,58 @@
 
 SCRIPT_PATH=$(cd "$(dirname "$0")"; pwd);
 
-sendPr(){
-  git push origin ${source_branch}
-  node ${SCRIPT_PATH}/gitlab.js --action sendPr --title "${TITLE}" --target "${target_branch}" --source "${source_branch}" --assignedTo "${assignee_id}"
+build(){
+  node ${SCRIPT_PATH}/docker.js --action build --dev ${dev} --imageName "${imageName}"
+}
+push(){
+  node ${SCRIPT_PATH}/docker.js --action push -f "${file}" --imageName "${imageName}" -s "${server}" -u username -p password
 }
 
 usage(){
   echo "Usage: automate-docker [command] [options]"
+  #NOTE: Indent using space to avoid breaking format in command line
   echo "Arguments:
-    build                                 command: send pull request
-    -T | --title [title]                    merge request title
-    -s | --source_branch [source_branch]    source branch name
-    -t | --target_branch [target_branch]    target branch name
-    -a | --assignee_id [assignee_id]        id of assigned user
+    build                                 command: build Docker image
+    -d | --dev                            image name as dev
+    -n | --imageName [imagename]          image name as [imagename]
+
+    push                                  command: push to Docker registry
+    -u | --username [imagename]           hub username
+    -p | --password [password]            hub password
+    -s | --server [server]                hub server
+    -f | --file [dockerfile]              hub dockerfile
+    -n | --imageName [imagename]          image name as [imagename]
+
     -h | --help if show help"
 }
 
 while [ "$1" != "" ]; do
     case $1 in
         build)                  action='build'
-						                    ;;
+                                ;;
+        -d | --dev)        			shift # past argument
+                                shift # past value
+                                dev=true
+                                ;;
+        -n | --imageName)       shift
+                                imageName=$1
+                                ;;
+    esac
+    case $1 in
+        push)                   action='push'
+                                ;;
         -u | --username)        shift
                                 username=$1
                                 ;;
         -p | --password)        shift
                                 password=$1
                                 ;;
+        -f | --file)            shift
+                                file=$1
+                                ;;
         -s | --server)          shift
                                 server=$1
                                 ;;
-        # if it's -h or --help, call function usage to show the help text
         -h | --help )           usage
                                 ;;
     esac
@@ -40,5 +62,10 @@ done
 
 if [ "${action}" = 'build' ];
 then
-  echo $username $password $server
+  echo $dev $imageName
+	build
+elif [ "${action}" = 'push' ];
+then
+  echo $username $password $file $server
+	push
 fi
